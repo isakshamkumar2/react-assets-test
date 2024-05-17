9ff638d6bc513cda8ade38279c867807cd782b2d
isakshamkumar2 <saksham@genflowly.com>
2024-05-17 08:30:30 +0530

    Remove .npmrc from tracking and update .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.ts',
+  entry: path.join(__dirname,'./src/index.ts'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
